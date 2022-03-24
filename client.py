883f415fa28b923efaf4bbe18d7e345399d9090a
Kalki Eshwar D <77596443+RoughRider7@users.noreply.github.com>
2022-03-24 14:28:47 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,37 +1,41 @@
 import threading
 import socket
 
-HOST = '**********'    # Add the ip address of the server you would like to connect to.
+HOST = '192.168.0.163'
 PORT = 55555
 FORMAT = 'utf-8'
 USERNAME = input("Enter your user name: ")
+END = False
+
+
+def receive_msg():
+    global END
+    while not END:
+        print(client_socket.recv(1024).decode(FORMAT))
+
+
+def send_msg():
+    global END
+    while not END:
+        message = input()
+        if message == '!QUIT':
+            close()
+        else:
+            client_socket.send(message.encode(FORMAT))
+
+
+def close():
+    global END
+    END = True
+    client_socket.send('!QUIT'.encode(FORMAT))
+    print('[COMPUTER]: Connection to server terminated.')
+
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 print('[COMPUTER]: Connecting to server')
 client_socket.send(USERNAME.encode(FORMAT))
-
-def receive_msg():
-    while True:
-        print(client_socket.recv(1024).decode(FORMAT))
-
-
-def send_msg():
-    while True:
-        message = input()
-        if message == '!QUIT':
-            close()
-
-
-receive_msg_thread = threading.Thread(target=receive_msg)
+receive_msg_thread = threading.Thread(target=receive_msg,)
 receive_msg_thread.start()
-
-send_msg_thread = threading.Thread(target=send_msg)
+send_msg_thread = threading.Thread(target=send_msg,)
 send_msg_thread.start()
-
-
-def close():
-    receive_msg_thread.join()
-    send_msg_thread.join()
-    client_socket.send('!QUIT'.encode(FORMAT))
-    print('[COMPUTER]: Connection to server terminated.')
