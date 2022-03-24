1f69f7f12f671c5d3c8ec9557e156c0d94ac6776
Kalki Eshwar D <77596443+RoughRider7@users.noreply.github.com>
2022-03-24 14:26:19 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,3 @@
-# [NOTE] CODE not working still trying to figure out what I'm missing.
-
 import socket
 import threading
 
@@ -26,14 +24,36 @@ def receive_msg(client, username):
                 USER_NAMES.remove(username)
                 broadcast(f'{username} has left the chat room.', '[SERVER]')
                 break
-            else:
-                broadcast(message, username)
+            broadcast(message, username)
         except:
             try:
                 client.close()
                 broadcast(f'{username} has left the chat room.', '[SERVER]')
                 print(f'[SERVER]: {username} has left the server.')
             except:
+                pass
+
+
+def broadcast(message, username):
+    for client in CLIENTS:
+        client.send(f'{username}: {message}'.encode(FORMAT))
+
+
+def on_boarding():
+    while True:
+        connection, address = server_socket.accept()
+        CLIENTS.append(connection)
+        connection.send('[SERVER]: Connected to server.\n'.encode(FORMAT))
+        user_name = connection.recv(1024).decode()
+        print(f'{user_name} with address {address} has connected to the server.')
+        USER_NAMES.append(user_name)
+        broadcast(f'{user_name} has joined the server.', '[SERVER]')
+        receive_msg_thread = threading.Thread(target=receive_msg, args=(connection, user_name,))
+        receive_msg_thread.start()
+
+
+on_boarding()
+
                 break
 
 
